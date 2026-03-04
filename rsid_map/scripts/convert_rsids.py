7a97fbca4355f0dea324b255cd1015f7c58a1f80
pete <pietro.dellabriottaparolo@gmail.com>
2026-03-04 11:05:16 +0200

    added rsid mapping

---
+++ rsid_map/scripts/convert_rsids.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+import argparse,os,gzip,itertools,subprocess,shlex,pickle
+import numpy as np
+import os,gzip
+from collections import defaultdict as dd
+from utils import file_exists,return_open_func,make_sure_path_exists,identify_separator,basic_iterator,get_path_info,return_header,load_rsid_mapping
+
+
+def check_inputs(args):
+    """
+    Sanitzes inputs
+    """
+    open_func = return_open_func(args.file)
+    # read header no matter what for debug perposes
+    separator = identify_separator(args.file)
+    header = return_header(args.file)
+
+    #check if metadata is integers
+    int_meta_check = np.prod([elem.isdigit() for elem in args.metadata])
+
+    # adjust columns if one wants to keep all file
+    if args.columns == 'all':
+        if int_meta_check:
+            args.columns = list(map(str,range(len(header))))
+        else:
+            args.columns = header
+
+    # check if output columns are integers
+    int_col_check=  np.prod([elem.isdigit() for elem in args.columns])
+
+    #both meta and out columns need to be consistent
+    if int_meta_check ^ int_col_check:
+        raise ValueError('both metadata and columns need to be of the same type')
+
+    # check that index is within limit
+    if int_meta_check and int_col_check:
+        args.columns = [int(elem) for elem in args.columns]
+        for index in args.columns:
+            assert index < len(header),f'{index} column index out of bounds'
+        out_index = args.columns
+        args.metadata = [int(elem) for elem in args.metadata]
+        for index in args.metadata:
+            assert index < len(header),f'{index} metadata index out of bounds'
+        meta_index = args.metadata
+
+    # check that columns and metadat names are in header
+    if (not int_meta_check) and (not int_col_check):
+        for column in args.columns:
+            assert column in header,f"{column} column not in header"
+        for column in args.metadata:
+            assert column in header,f"{column} metadata not in header"      
+
+        # index of columns to write out
+        out_index = [header.index(elem) for elem in args.columns]
+        # index of columns to read
+        meta_index = [header.index(elem) for elem in args.metadata]
+
+    print('columns to keep',out_index)
+    print('metadata columns',meta_index)
+    return out_index,meta_index,header,separator
+
+def parse_file(args):
+
+    _,out_root,extension = get_path_info(args.file)
+
+    out_index,meta_index,header,separator = check_inputs(args)
+    write_func = gzip.open if args.gz else open
+
+    out_file = os.path.join(args.out,out_root + ".CONVERT")
+    if args.gz: out_file += '.gz'
+
+    #if there is no header, don't skip first row when parsing file
+    skip = 0 if args.no_header else 1
+    iterator = basic_iterator(args.file,skiprows=skip)
+    if args.to_rsid:
+        rsid_dict = load_rsid_mapping(args.map,inverse = True)#chrompos --> rsid
+        out_file = out_file.replace('CONVERT','rsid')
+        print(f"saving to {out_file}")
+        with write_func(out_file,'wt') as o:
+            if not args.no_header:# write header,since it cannot be parsed
+                o.write(separator.join([header[i] for i in out_index]) + '\n')
+            for line in iterator:
+                #extract chrom/pos from snp id (i.e. the first element of meta_index)
+                snp = line[meta_index[0]]
+                # return all integers in snp string. i assume the first 2 are chrom/pos
+                integers = ''.join((ch if ch.isdigit() else ' ') for ch in snp).split()
+                if 'X' in snp: # unless it's chrom X
+                    chrom,pos = 'X',integers[0]
+                else:
+                    chrom,pos,*_ = integers
+                    
+                rsid = rsid_dict['_'.join([chrom,pos])]
+                if rsid: # replace snpid with rsid
+                    line[meta_index[0]] = rsid
+                o.write(separator.join([line[i] for i in out_index]) + '\n')
+            
+    if args.to_chrompos:
+        chrompos_dict = load_rsid_mapping(args.map) # risd -- >chrompos
+        out_file = out_file.replace('CONVERT','chrompos')
+        print(f"saving to {out_file}")
+        with write_func(out_file,'wt') as o:
+            if not args.no_header:
+                o.write(separator.join([header[i] for i in out_index]) + '\n')
+            for line in iterator:
+                rsid,a1,a2 = [line[i] for i in meta_index]
+                chrompos = chrompos_dict[rsid]
+                if chrompos:
+                    line[meta_index[0]] = f"chr{chrompos}_{a1}_{a2}"
+                o.write(separator.join([line[i] for i in out_index]) + '\n')
+
+
+if __name__ == '__main__':
+
+    """
+    Pipeline to convert any file from/to rsids to chrompos notation
+    """
+    parser = argparse.ArgumentParser(description ="Process a file")
+
+    #Basic inputs
+    parser.add_argument("-o",'--out',type = str, help = "folder in which to save the results", required = True)
+    parser.add_argument("--file",'-f',type = file_exists, help = "File to map", required = True)             
+    parser.add_argument("--map",type = file_exists, help = "Mapping file from rsid to chrompos", required = True)             
+    parser.add_argument('--gz',action = 'store_true',help = ' Compress output file to gz',default = False)
+    parser.add_argument('--no-header',action = 'store_true',help = 'Flag to use when no header is present',default = False)
+    parser.add_argument('--metadata','-m',nargs = '*',required = True,help='columns required for parsing. Should be SNPID columns for to-chrompos and SNPID,A1,A2 to go to rsid')    
+    parser.add_argument('--columns',help = 'column that need to be kept, either numerical integers or column names',action = 'store',type = str,nargs = '*', default = 'all')
+    
+    conv_type = parser.add_mutually_exclusive_group(required = True)
+    conv_type.add_argument('--to-rsid',action = 'store_true')
+    conv_type.add_argument('--to-chrompos',action = 'store_true')
+        
+    args = parser.parse_args()
+    print(args)
+    make_sure_path_exists(args.out)
+    args.file = os.path.abspath(args.file)
+    parse_file(args)
---
+++ rsid_map/scripts/utils.py
@@ -0,0 +1,324 @@
+import os,mmap,sys,subprocess,csv,gzip,pickle,shlex,time
+from tempfile import NamedTemporaryFile
+from functools import partial
+from collections import defaultdict as dd
+import numpy as np
+import multiprocessing,csv 
+cpus = multiprocessing.cpu_count()
+
+mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')  # e.g. 4015976448
+mem_mib = mem_bytes/(1024.**2) 
+proc_mem = mem_mib / (cpus +1)
+
+allele_dict = dd(str)
+for a1,a2 in [('T','A'),('C','G')]:
+    allele_dict[a1] = a2
+    allele_dict[a2] = a1
+
+
+def map_alleles(a1,a2):
+    """
+    Flips alleles to the A strand if neccessary and orders them lexicogaphically
+    """
+    # check if the A variant is present
+    if 'A' not in a1 + a2 and 'a' not in a1+a2:
+        # for both/ref and alt map them to the A strand and order each one lexicographically
+        a1 = flip_strand(a1)
+        a2 = flip_strand(a2)
+    # further sorting
+    return sorted([a1,a2])
+
+
+def flip_strand(allele):
+    return ''.join([allele_dict[elem.upper()] for elem in allele])
+
+def make_sure_path_exists(path):
+    import errno
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise                
+
+def file_exists(fname):
+    '''
+    Function to pass to type in argparse
+    '''
+    if os.path.isfile(fname):
+        return str(fname)
+    else:
+        print(fname + ' does not exist')
+        sys.exit(1)
+        
+def mapcount(filename):
+
+    if not os.path.isfile(filename):
+        raise ValueError("File doesn't exist")
+    
+    try:
+        return count_lines(filename)
+    except:
+        return 0
+    
+def count_lines(filename):
+    '''
+    Counts line in file
+    '''
+    f = open(filename, "r+")
+    buf = mmap.mmap(f.fileno(), 0)
+    lines = 0
+    readline = buf.readline
+    while readline():
+        lines += 1
+    return lines
+
+def tmp_bash(cmd,check = False):
+    
+
+    scriptFile = NamedTemporaryFile(delete=True)
+    with open(scriptFile.name, 'w') as f:
+        f.write("#!/bin/bash\n")
+        f.write(cmd + "\n")
+
+    os.chmod(scriptFile.name,0o777)
+    scriptFile.file.close()
+
+    if check:
+        subprocess.check_call(scriptFile.name)
+    else:
+        subprocess.call(scriptFile.name,stderr = subprocess.DEVNULL)
+
+
+def return_open_func(f):
+    '''
+    Detects file extension and return proper open_func
+    '''
+   
+    file_path,file_root,file_extension = get_path_info(f)
+
+    if 'bgz' in file_extension:
+        #print('gzip.open with rb mode')
+        open_func = partial(gzip.open, mode = 'rb')
+    
+    elif 'gz' in file_extension:
+        #print('gzip.open with rt mode')
+        open_func = partial(gzip.open, mode = 'rt')
+
+    else:
+        #print('regular open')
+        open_func = open      
+    return open_func
+
+def get_path_info(path):
+    file_path = os.path.dirname(path)
+    basename = os.path.basename(path)
+    file_root, file_extension = os.path.splitext(basename)
+    return file_path,file_root,file_extension
+
+def identify_separator(f):
+    open_func = return_open_func(f)
+    with open_func(f) as i:header = i.readline().strip()
+    sniffer = csv.Sniffer()
+    dialect = sniffer.sniff(header)
+    return dialect.delimiter
+    
+def get_filepaths(directory):
+    """
+    This function will generate the file names in a directory 
+    tree by walking the tree either top-down or bottom-up. For each 
+    directory in the tree rooted at directory top (including top itself), 
+    it yields a 3-tuple (dirpath, dirnames, filenames).
+    """
+    file_paths = []  # List which will store all of the full filepaths.
+
+    # Walk the tree.
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            # Join the two strings in order to form the full filepath.
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)  # Add it to the list.
+
+    return file_paths  # Self-explanatory.
+
+
+def basic_iterator(f, separator=None, skiprows=0, count=False, columns='all'):
+    '''
+    Iterates through a file (plain or gzipped), splitting each line by `separator` and
+    extracting specified columns.
+    '''
+    open_func = return_open_func(f)
+    if not separator:
+        separator = identify_separator(f)
+    i = open_func(f)
+    for _ in range(skiprows):
+        next(i)
+    if count is False:
+        for line in i:
+            line = line.strip().split(separator)
+            line = return_columns(line, columns)
+            yield line
+    else:
+        row = 0
+        for line in i:
+            line = line.strip().split(separator)
+            line = return_columns(line, columns)
+            row += 1
+            yield row, line
+
+def return_columns(l,columns):
+    '''
+    Returns all columns, or rather the elements, provided the columns
+    '''
+    if columns == 'all':
+        return l
+    elif type(columns) == int:
+        return l[columns]
+    elif type(columns) == list:
+        return list(map(l.__getitem__,columns))
+
+def return_header(f):
+
+    open_func = return_open_func(f)
+    with open_func(f) as i:header = i.readline().strip()
+    delimiter = identify_separator(f)
+    header = header.split(delimiter)
+    return header
+        
+        
+def fix_header(file_path):
+    """
+    Strip excess spaces if needed
+    """
+    #identify separator and fix header if necessary
+    header = return_header(file_path)
+    header_fix = []
+    for elem in header:
+        if '  ' in elem:
+            while '  ' in elem: elem = elem.replace('  ',' ')
+        if ' ' in elem:
+            for s in elem.split(' '): header_fix.append(s)
+        else:
+            header_fix.append(elem)
+    return header_fix
+
+       
+def isfloat(value):
+    try:
+        # return True if value is a float and NOT infinite
+        value =float(value)
+        return ~np.isinf(value)
+
+    except ValueError:
+        return False
+
+
+def pretty_print(string,l = 30):
+    l = l-int(len(string)/2)
+    print('-'*l + '> ' + string + ' <' + '-'*l)
+    
+
+def mapcount_gzip(filename):
+    if not os.path.isfile(filename):
+        raise ValueError("File doesn't exist")
+    try:
+        return count_gzip(filename)
+    except:
+        return 0
+    
+def count_gzip(myfile):
+
+    scriptFile = NamedTemporaryFile(delete=True)
+    with open(scriptFile.name, 'w') as f:
+        tmp_bash(f"zcat {myfile} | wc -l > {scriptFile.name} ")
+    return int(open(scriptFile.name).read())  
+
+def progressBar(value, endvalue, bar_length=20):
+    '''
+    Writes progress bar, given value (eg.current row) and endvalue(eg. total number of rows)
+    '''
+
+    percent = float(value) / endvalue
+    arrow = '-' * int(round(percent * bar_length)-1) + '>'
+    spaces = ' ' * (bar_length - len(arrow))
+
+    sys.stdout.write("\rPercent: [{0}] {1}%".format(arrow + spaces, int(round(percent * 100))))
+    sys.stdout.flush()
+
+#def load_pos_mapping(chrompos_map):
+    '''
+    Loads the chrom_pos to ref/alt mapping for finngen
+    '''
+    out_pickle = chrompos_map + '.pickle'
+    if os.path.isfile(out_pickle):
+        print('loading chrompos dict -->', out_pickle)
+
+        with open(out_pickle,'rb') as i: pos_dict = pickle.load(i)
+    else:
+        pos_dict = dd(list)
+        print('generating chrompos dict -->', out_pickle)
+        iterator = basic_iterator(chrompos_map)
+        for entry in iterator:
+            chrom_pos,ref,alt = entry
+            pos_dict[chrom_pos].append([ref,alt])
+        with open(out_pickle,'wb') as o:
+            pickle.dump(pos_dict,o,protocol = pickle.HIGHEST_PROTOCOL)
+    print('done.')
+    return pos_dict
+
+#def load_rsid_mapping(rsid_map,inverse = False):
+    '''
+    Loads the chrompos to rsid mapping
+    '''
+    out_pickle = os.path.join(rsid_map) + '.pickle'
+    if inverse:
+        out_pickle += '.chrompos'
+    if os.path.isfile(out_pickle):
+        print('loading rsid dict -->', out_pickle)
+        with open(out_pickle,'rb') as i: rsid_dict = pickle.load(i)
+    else:
+        print('generating rsid dict --> ',out_pickle)
+        rsid_dict = dd(str)
+        header = return_header(rsid_map)
+        rsid_col = [header.index(elem) for elem in header if 'rs' in elem][0]
+        columns = [0,1] if not rsid_col else [1,0]
+        if inverse: columns = columns[::-1]
+        
+        for entry in basic_iterator(rsid_map,columns = columns):
+            rsid,chrom_pos = entry
+            rsid_dict[rsid] = chrom_pos
+        with open(out_pickle,'wb') as o:
+            pickle.dump(rsid_dict,o,protocol = pickle.HIGHEST_PROTOCOL)
+        print('done.')
+    return rsid_dict
+
+
+
+
+def natural_sort(l):
+    import re
+    convert = lambda text: int(text) if text.isdigit() else text.lower() 
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(l, key = alphanum_key)
+
+def timing_function(some_function):
+
+    """
+    Outputs the time a function takes  to execute.
+    """
+
+    def wrapper(*args,**kwargs):
+        t1 = time.time()
+        some_function(*args)
+        t2 = time.time()
+        print("Time it took to run the function: " + str((t2 - t1)))
+
+    return wrapper
+
+
+
+def merge_files(o_file,file_list):
+    with open(o_file,'wt') as o:
+        for f in file_list:
+            with open(f,'rt') as i:
+                for line in i:
+                    o.write(line)
